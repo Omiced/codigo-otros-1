d0704bbadb763207d92e3e9195f0216ba596ecba
Josue Cano <73917299+Omiced@users.noreply.github.com>
2023-01-18 16:40:33 +0000

    terminado ejercicio creo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector(".name");
+const b = document.querySelector(".blog");
+const uno = document.querySelector(".location");
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+//es un programa que realiza una petición a la api de github para buscar un usuario
+//luego muestra 3 datos de este usuario.
+//arreglado obtención de elementos en el dom, nombres de variable
+//peticion y obtención de datos
+//se puso asyn cn la función para poder usar await.
+async function displayUser(username) {
+  n.textContent = "cargando...";
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json();
+  n.textContent = data.name;
+  b.textContent = data.blog;
+  uno.textContent = data.location;
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("stolinski").catch(handleError);
